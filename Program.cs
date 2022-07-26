c29cdf9d44b4cb33723d91c83bc00fd0173e0c40
Slavik191079 <slavik191079@gmail.com>
2022-07-26 12:00:12 +0300

    add Programm file

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+string[] myArray = {"cat", "876", "admin", "!", "ball"}; 
+int elementLength = 3; 
+ 
+PrintArray(FindShortElements(myArray, elementLength)); 
+ 
+string[] FindShortElements(string[] inputArray,int number) 
+{ 
+    int CountShortElements(string[] inputArray) 
+    { 
+        int result = 0; 
+        for(int i = 0; i < inputArray.Count(); i++) 
+        { 
+            if(inputArray[i].Length <= number) result++; 
+        } 
+    return result; 
+    } 
+ 
+    string[] outputArray = new string[CountShortElements(inputArray)]; 
+    int index = 0; 
+    for(int i = 0; i < inputArray.Length; i++) 
+    { 
+        if(inputArray[i].Length <= number) 
+        { 
+            outputArray[index] = inputArray[i]; 
+            index += 1; 
+        } 
+    } 
+    return outputArray; 
+}
+
+void PrintArray(string[] array)  
+{ 
+   for(int i = 0; i < array.Length; i++) 
+  Console.WriteLine(array[i]); 
+}
